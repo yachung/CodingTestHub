d4f7ef49457633a5d6715b3cdda43a51d1979e3b
yachung <59554338+yachung@users.noreply.github.com>
2024-03-26 19:03:05 +0900

    [Silver IV] Title: solved.ac, Time: 116 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.cs
+++ 백준/Silver/18110. solved．ac/solved．ac.cs
@@ -6,11 +6,12 @@ class Program
     {
         int input = int.Parse(Console.ReadLine());
 
-        int outlier;
-        double trimAvg = 0;
-
-        if (input != 0)
+        if (input == 0)
+            Console.WriteLine(0);
+        else
         {
+            int outlier;
+            double trimAvg = 0;
             int[] rateArr = new int[input];
 
             for (int i = 0; i < input; ++i)
@@ -18,26 +19,19 @@ class Program
 
             Array.Sort(rateArr);
 
-            outlier = CustomRound(rateArr.Length * 0.15);
+            outlier = CustomRound(rateArr.Length * 0.15f);
 
             for (int i = outlier; i < rateArr.Length - outlier; ++i)
                 trimAvg += rateArr[i];
 
             trimAvg = CustomRound(trimAvg / (rateArr.Length - 2 * outlier));
-        }
 
-        Console.WriteLine(trimAvg);
+            Console.WriteLine(trimAvg);
+        }
     }
 
     static int CustomRound(double num)
     {
-        int value = (int)num;
-
-        num = num - value;
-
-        if (num < 0.5)
-            return value;
-        else
-            return ++value;
+        return (num - (int)num) < 0.5f ? (int)num : (int)num + 1;
     }
 }
