07feba0d8027cc8eb51dd51f3330653d0880490a
yachung <59554338+yachung@users.noreply.github.com>
2024-04-04 22:32:53 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 5764 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        int input = int.Parse(Console.ReadLine());
+        int[] tcArr = new int[input];
+
+        int max = 5;
+        
+        for (int i = 0; i < input; ++i)
+        {
+            tcArr[i] = int.Parse(Console.ReadLine());
+            max = Math.Max(max, tcArr[i]);
+        }
+
+        // 1 <= tc <= 100 이라 배열이 int 면 tc가 100일때 오버플로우 발생.
+        long[] tcSum = new long[max + 1];
+
+        // 디폴트 값 설정
+        tcSum[1] = 1;
+        tcSum[2] = 1;
+        tcSum[3] = 1;
+        tcSum[4] = 2;
+        tcSum[5] = 2;
+
+        for (int i = 6; i < tcSum.Length; ++i)
+            tcSum[i] = tcSum[i - 1] + tcSum[i - 5];
+
+        foreach (var item in tcArr)
+            sb.Append(tcSum[item]).Append('\n');
+
+        Console.WriteLine(sb);
+    }
+}
