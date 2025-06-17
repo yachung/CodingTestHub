ce86e800e846ba491168b19d624a3d731d8f647c
yachung <59554338+yachung@users.noreply.github.com>
2025-06-18 00:40:46 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 80 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -5,15 +5,8 @@ queue = deque(range(1, N + 1))
 result = []
 
 for _ in range(N):
-  for i in range(K):
-    value = queue.popleft()
-    if i == K - 1:
-      result.append(value)
-    else:
-      queue.append(value)
-      
-print("<",end="")
-for i in range(N-1):
-    print(result[i],end=", ")
-print(result[N-1], end = "")
-print(">",end="")
+  for i in range(K-1):
+    queue.append(queue.popleft())
+  result.append(str(queue.popleft()))
+
+print("<"+", ".join(result)+">")
