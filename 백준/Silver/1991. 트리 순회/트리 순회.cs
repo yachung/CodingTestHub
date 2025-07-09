8758a006da93f76c3786dc0f015f351425ce89d0
yachung <59554338+yachung@users.noreply.github.com>
2025-07-10 01:04:31 +0900

    [Silver I] Title: 트리 순회, Time: 56 ms, Memory: 5872 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cs
+++ 백준/Silver/1991. 트리 순회/트리 순회.cs
@@ -3,7 +3,7 @@ using System.Xml.Linq;
 
 public class Solution
 {
-    static List<char> visited = new List<char>();
+    const char root = 'A';
     static Dictionary<char, List<char>> graph = new Dictionary<char, List<char>>();
 
     static void Main(string[] args)
@@ -14,61 +14,25 @@ public class Solution
         var sw = new StreamWriter(Console.OpenStandardOutput());
 
         int N = int.Parse(sr.ReadLine());
-        
 
         for (int i = 0; i < N; i++)
         {
             char[] input = Array.ConvertAll(sr.ReadLine().Split(), char.Parse);
-            
+
             graph.Add(input[0], new List<char> { input[1], input[2] });
         }
 
-        Preorder('A');
-        foreach (var item in visited)
-            sb.Append(item);
-        sb.AppendLine();
-        visited = new List<char>();
-        Inorder('A');
-        foreach (var item in visited)
-            sb.Append(item);
-        sb.AppendLine();
-        visited = new List<char>();
-        Postorder('A');
-        foreach (var item in visited)
-            sb.Append(item);
+        sb.AppendLine(Preorder(root));
+        sb.AppendLine(Inorder(root));
+        sb.AppendLine(Postorder(root));
 
         sw.WriteLine(sb.ToString());
 
         sw.Close();
+        sr.Close();
     }
 
-    public static void Preorder(char node)
-    {
-        if (!graph.ContainsKey(node))
-            return;
-
-        visited.Add(node);
-        Preorder(graph[node][0]);
-        Preorder(graph[node][1]);
-    }
-
-    public static void Inorder(char node)
-    {
-        if (!graph.ContainsKey(node))
-            return;
-
-        Inorder(graph[node][0]);
-        visited.Add(node);
-        Inorder(graph[node][1]);
-    }
-
-    public static void Postorder(char node)
-    {
-        if (!graph.ContainsKey(node))
-            return;
-
-        Postorder(graph[node][0]);
-        Postorder(graph[node][1]);
-        visited.Add(node);
-    }
+    public static string Preorder(char node) => node.Equals('.') ? "" : $"{node}{Preorder(graph[node][0])}{Preorder(graph[node][1])}";
+    public static string Inorder(char node) => node.Equals('.') ? "" : $"{Inorder(graph[node][0])}{node}{Inorder(graph[node][1])}";
+    public static string Postorder(char node) => node.Equals('.') ? "" : $"{Postorder(graph[node][0])}{Postorder(graph[node][1])}{node}";
 }
